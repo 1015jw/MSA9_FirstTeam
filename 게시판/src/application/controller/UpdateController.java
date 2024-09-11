abd55434a46418712d9c2f42cd83f7ac21735657
Bong127 <ansqhdgus200@naver.com>
2024-09-11 18:24:20 +0900

    수정 레이아웃

---
+++ 게시판/src/application/controller/UpdateController.java
@@ -0,0 +1,41 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UpdateController {
+
+    @FXML
+    private TextField outContent;
+
+    @FXML
+    private TextField outNo;
+
+    @FXML
+    private Label outReg;
+
+    @FXML
+    private TextField outTitle;
+
+    @FXML
+    private Label outUpd;
+
+    @FXML
+    private Label outView;
+
+    @FXML
+    private TextField outWriter;
+
+    @FXML
+    void toList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
+
+}
