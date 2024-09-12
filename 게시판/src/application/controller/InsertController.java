6fb445cbb75f730832d5f54714f54da089f49160
1015jw <971015jw@gmail.com>
2024-09-12 11:13:35 +0900

    게시글 작성

---
+++ 게시판/src/application/controller/InsertController.java
@@ -0,0 +1,37 @@
+package application.controller;
+
+import application.Main;
+import application.DTO.Board;
+import application.Service.BoardService;
+import application.Service.BoardServiceImpl;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class InsertController {
+
+    @FXML
+    private TextField contentBox;
+
+    @FXML
+    private TextField titleBox;
+
+    @FXML
+    private TextField writerBox;
+
+    @FXML
+    void toList(ActionEvent event) throws Exception {
+        Main.setRoot("UI/List");
+    }
+
+    @FXML
+    void insert(ActionEvent event) throws Exception {
+        Board board = new Board();
+        board.setTitle(titleBox.getText());
+        board.setWriter(titleBox.getText());
+        board.setContent(titleBox.getText());
+        BoardService service = new BoardServiceImpl();
+        service.insert(board);
+        Main.setRoot("UI/List");
+    }
+}
